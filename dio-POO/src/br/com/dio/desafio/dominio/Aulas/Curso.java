9dce416f269b2186d32c8b72824cbf0aa64bf1e9
Gabriel Schütt de Almeida <59267432+gabrielschut@users.noreply.github.com>
2024-08-23 20:24:27 -0300

    add POO challenge

---
+++ dio-POO/src/br/com/dio/desafio/dominio/Aulas/Curso.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio.Aulas;
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo= " + getTitulo() + '\n' +
+                ", descricao=" + getDescricao() + '\n' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
